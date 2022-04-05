818461b9af9e4f40974a345a6aa1c516fb78e41b
Kaius Loos <kaius.loos@gmail.com>
2022-04-05 13:48:38 +0300

    Improve handling of LapInterpolators. Replacements  now point to the existing interpolator not build one themselves.

--- src/ksBroadcastingNetwork/Structs/TrackData.cs
+++ src/ksBroadcastingNetwork/Structs/TrackData.cs
@@ -23,43 +23,36 @@ namespace KLPlugins.Leaderboard.ksBroadcastingNetwork.Structs
         public string TrackName { get; internal set; }
         public TrackType TrackId { get; internal set; }
         public float TrackMeters { get; internal set; }
-        //public Dictionary<string, List<string>> CameraSets { get; internal set; }
-        //public IEnumerable<string> HUDPages { get; internal set; }
-
         public static CarClassArray<LapInterpolator> LapInterpolators = null;
 
         /// <summary>
         /// Read default lap data for calculation of gaps.
         /// </summary>
         public static void ReadDefBestLaps() {
-            if (LapInterpolators != null) return;
-
+            if (LapInterpolators != null) return; // We have already initialized
             LapInterpolators = new CarClassArray<LapInterpolator>(null);
 
-            AddLapInterpolator(CarClass.GT3, new CarClass[] { });
-            AddLapInterpolator(CarClass.GT4, new CarClass[] { });
-            AddLapInterpolator(CarClass.TCX, new CarClass[] { });
-            AddLapInterpolator(CarClass.CUP21, new CarClass[] { CarClass.CUP17, CarClass.ST21, CarClass.ST15, CarClass.CHL });
-            AddLapInterpolator(CarClass.CUP17, new CarClass[] { CarClass.CUP21, CarClass.ST21, CarClass.ST15, CarClass.CHL });
-            AddLapInterpolator(CarClass.ST15, new CarClass[] { CarClass.ST21, CarClass.CUP17, CarClass.CUP21, CarClass.CHL });
-            AddLapInterpolator(CarClass.ST21, new CarClass[] { CarClass.ST15, CarClass.CUP17, CarClass.CUP21, CarClass.CHL });
-            AddLapInterpolator(CarClass.CHL, new CarClass[] { CarClass.ST21, CarClass.CUP21, CarClass.CUP17,  CarClass.ST15 });
+            AddLapInterpolator(CarClass.GT3);
+            AddLapInterpolator(CarClass.GT4);
+            AddLapInterpolator(CarClass.TCX);
+            AddLapInterpolator(CarClass.CUP21);
+            AddLapInterpolator(CarClass.CUP17);
+            AddLapInterpolator(CarClass.ST15);
+            AddLapInterpolator(CarClass.ST21);
+            AddLapInterpolator(CarClass.CHL);
+
+            SetReplacements(CarClass.GT3, new CarClass[] { CarClass.CUP21, CarClass.ST21, CarClass.CUP17, CarClass.ST15, CarClass.CHL });
+            SetReplacements(CarClass.CUP21, new CarClass[] { CarClass.CUP17, CarClass.ST21, CarClass.ST15, CarClass.CHL, CarClass.GT3 });
+            SetReplacements(CarClass.CUP17, new CarClass[] { CarClass.CUP21, CarClass.ST21, CarClass.ST15, CarClass.CHL, CarClass.GT3 });
+            SetReplacements(CarClass.ST21, new CarClass[] { CarClass.CUP21, CarClass.CUP17, CarClass.ST15, CarClass.CHL, CarClass.GT3 });
+            SetReplacements(CarClass.ST15, new CarClass[] { CarClass.ST21, CarClass.CUP21, CarClass.CUP17,  CarClass.CHL, CarClass.GT3 });
         }
 
-        private static void AddLapInterpolator(CarClass cls, CarClass[] replacements) {
+        private static void AddLapInterpolator(CarClass cls) {
             var fname = $"{LeaderboardPlugin.Settings.PluginDataLocation}\\laps\\{(int)Values.TrackData.TrackId}_{cls}.txt";
             if (!File.Exists(fname)) {
-                foreach (var replacement in replacements) {
-                    fname = $"{LeaderboardPlugin.Settings.PluginDataLocation}\\laps\\{(int)Values.TrackData.TrackId}_{replacement}.txt";
-                    if (File.Exists(fname)) {
-                        break;
-                    }
-                }
-
-                if (!File.Exists(fname)) {
-                    LeaderboardPlugin.LogInfo($"Couldn't build lap interpolator for {cls} because no suitable track data exists.");
-                    return;
-                }; 
+                LeaderboardPlugin.LogInfo($"Couldn't build lap interpolator for {cls} because no suitable track data exists.");
+                return;
             }
 
             var pos = new List<double>();
@@ -67,6 +60,7 @@ namespace KLPlugins.Leaderboard.ksBroadcastingNetwork.Structs
             try {
                 foreach (var l in File.ReadLines(fname)) {
                     if (l == "") continue;
+                    // Data order: splinePositions, laptime in ms, speed in kmh
                     var splits = l.Split(';');
                     double p = float.Parse(splits[0]);
                     var t = double.Parse(splits[1]) / 1000.0;
@@ -79,7 +73,18 @@ namespace KLPlugins.Leaderboard.ksBroadcastingNetwork.Structs
             } catch (Exception ex) {
                 LeaderboardPlugin.LogError($"Failed to read {fname} with error: {ex}");
             }
-            
+        }
+
+        private static void SetReplacements(CarClass cls, CarClass[] replacements) {
+            if (LapInterpolators[cls] == null) {
+                foreach (var r in replacements) {
+                    if (LapInterpolators[r] != null) {
+                        LapInterpolators[cls] = LapInterpolators[r];
+                        return;
+                    }
+                }
+                LeaderboardPlugin.LogInfo($"Couldn't find replacement lap interpolator for {cls}.");
+            }
         }
     }
 }

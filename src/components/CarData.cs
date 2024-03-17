634334c414fce7d44a75249815cd1d6c04c9e5ed
Kaius Loos <kaius.loos@gmail.com>
2024-03-17 14:36:57 +0200

    feat: export lap properties directly available in SimHub data
    Looks like all of them are easily available.

--- src/components/CarData.cs
+++ src/components/CarData.cs
@@ -3,24 +3,49 @@ using GameReaderCommon;
 namespace KLPlugins.DynLeaderboards.Car {
 
     public class CarData {
-        public string DriverName => this._rawData.Name;
-        public string CarClass => this._rawData.CarClass;
-        public bool IsFocused => this._rawData.IsPlayer;
+        public string DriverName => this._rawDataNew.Name;
+
+        public string CarClass => this._rawDataNew.CarClass;
+
+        public int Laps => this._rawDataNew.CurrentLap ?? 0;
+        public double CurrentLapTime => this._rawDataNew.CurrentLapHighPrecision ?? double.NaN;
+        public bool IsCurrentLapOutLap => this._rawDataNew.PitOutAtLap == this.Laps;
+        public bool IsLastLapOutLap => this._rawDataNew.PitOutAtLap == this.Laps - 1;
+        public bool IsCurrentLapInLap => this._rawDataNew.PitEnterAtLap == this.Laps;
+        public bool IsLastLapInLap => this._rawDataNew.PitEnterAtLap == this.Laps - 1;
+        public bool IsCurrentLapValid => this._rawDataNew.LapValid;
+        public bool IsLastLapValid { get; private set; }
+        public SectorTimes LastLap => this._rawDataNew.LastLapSectorTimes;
+        public SectorTimes BestLap => this._rawDataNew.BestLapSectorTimes;
+        public SectorSplits BestSectors => this._rawDataNew.BestSectorSplits;
+
+        public bool IsFocused => this._rawDataNew.IsPlayer;
+
+
+
         public int PositionOverall { get; private set; }
         public int PositionInClass { get; private set; }
         public int IndexOverall => this.PositionOverall - 1;
         public int IndexClass => this.PositionInClass - 1;
 
-        private Opponent _rawData;
+        private Opponent _rawDataNew;
+        private Opponent _rawDataOld;
 
         public CarData(Opponent rawData) {
-            this._rawData = rawData;
-            this.PositionOverall = this._rawData.Position;
-            this.PositionInClass = this._rawData.PositionInClass;
+            this._rawDataNew = rawData;
+            this.Update(rawData);
+            this.PositionOverall = this._rawDataNew!.Position;
+            this.PositionInClass = this._rawDataNew.PositionInClass;
         }
 
         public void Update(Opponent rawData) {
-            this._rawData = rawData;
+            this._rawDataOld = this._rawDataNew;
+            this._rawDataNew = rawData;
+
+            if (this.Laps > this._rawDataOld.CurrentLap) {
+                // new lap
+                this.IsLastLapValid = this._rawDataOld.LapValid;
+            }
         }
 
         public void SetOverallPosition(int overall) {

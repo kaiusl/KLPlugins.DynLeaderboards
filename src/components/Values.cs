958299c3322344a1ff101fbe308fb3174f110d3c
Kaius Loos <kaius.loos@gmail.com>
2024-03-19 16:21:20 +0200

    refactor: make cars persistent eg don't create new car every time
    This will be necessary for some properties which need to be persistent across updates. For example is a car is finished.

--- src/components/Values.cs
+++ src/components/Values.cs
@@ -8,6 +8,8 @@ using KLPlugins.DynLeaderboards.Car;
 
 using SimHub.Plugins;
 using System.Linq;
+using KLPlugins.DynLeaderboards.Helpers;
+using System.Diagnostics;
 
 namespace KLPlugins.DynLeaderboards {
     /// <summary>
@@ -60,34 +62,81 @@ namespace KLPlugins.DynLeaderboards {
 
             this.Booleans.OnDataUpdate(data, this);
             this.Session.OnDataUpdate(data, this);
+            this.UpdateCars(data);
+        }
 
-            // TODO: don't create new CarData each time, update previous objects
-            this.OverallOrder.Clear();
-            string? playerClass = null;
-            var cars = data.NewData.Opponents.Select((c, i) => (new CarData(c), i));
-            foreach (var (car, i) in cars) {
-                this.OverallOrder.Add(car);
-                car.SetOverallPosition(i + 1);
+        private void UpdateCars(GameData data) {
+            IEnumerable<(Opponent, int)> cars;
+            if (DynLeaderboardsPlugin.Game.IsAcc) {
+                // In ACC the first car is "Me", which is weird ghost car that doesn't have any info
+                cars = data.NewData.Opponents.Skip(1).WithIndex();
+            } else {
+                cars = data.NewData.Opponents.WithIndex();
+            }
+
+            foreach (var (opponent, i) in cars) {
+                // Most common case is that the car's position hasn't changed
+                var car = this.OverallOrder.ElementAtOrDefault(i);
+                if (car == null || car.Id != opponent.Id) {
+                    car = this.OverallOrder.Find(c => c.Id == opponent.Id);
+                }
+
+                if (car == null) {
+                    car = new CarData(opponent);
+                    this.OverallOrder.Add(car);
+                } else {
+                    Debug.Assert(car.Id == opponent.Id);
+                    car.UpdateIndependent(opponent);
+                }
+
+                car.IsUpdated = true;
+
                 if (car.IsFocused) {
                     this.FocusedCar = car;
                 }
             }
 
-            this.ClassOrder.Clear();
-            var classCars = this.OverallOrder
-                .Where(c => c.CarClass == playerClass)
-                .Select((c, i) => (c, i));
-            foreach (var (car, i) in classCars) {
-                this.ClassOrder.Add(car);
-                car.SetClassPosition(i + 1);
+            // TODO: this is temporary, use completely custom sort later
+            this.OverallOrder.Sort((c1, c2) => {
+                var isUpdated = c2.IsUpdated.CompareTo(c1.IsUpdated);
+                // put cars that didn't receive update to the back
+                if (isUpdated != 0) {
+                    return isUpdated;
+                } else {
+                    return c1.RawDataNew.Position.CompareTo(c2.RawDataNew.Position); ;
+                }
+            });
+
+            // Remove cars that didn't receive update
+            var numNotUpdated = this.OverallOrder.AsEnumerable().Reverse().FirstIndex(c => c.IsUpdated);
+            if (numNotUpdated > 0) {
+                this.OverallOrder.RemoveRange(this.OverallOrder.Count - numNotUpdated, numNotUpdated);
             }
 
-            // Some parts of the update require that basic data on every car has been updated
-            if (this.FocusedCar != null) {
-                this.RelativeOnTrackAheadOrder.Clear();
-                this.RelativeOnTrackBehindOrder.Clear();
-                foreach (var car in this.OverallOrder) {
+            this.ClassOrder.Clear();
+            this.RelativeOnTrackAheadOrder.Clear();
+            this.RelativeOnTrackBehindOrder.Clear();
+            Dictionary<string, int> classPositions = [];
+            var focusedClass = this.FocusedCar?.CarClass;
+            foreach (var (car, i) in this.OverallOrder.WithIndex()) {
+                car.SetOverallPosition(i + 1);
+
+                if (car.CarClass == null) {
+                    DynLeaderboardsPlugin.LogError($"Car {car.Id} has no class");
+                    continue;
+                }
+
+                if (!classPositions.ContainsKey(car.CarClass)) {
+                    classPositions.Add(car.CarClass, 1);
+                }
+                car.SetClassPosition(classPositions[car.CarClass]++);
+
+                if (this.FocusedCar != null) {
                     car.UpdateDependsOnOthers(this.FocusedCar);
+                    if (car.CarClass == focusedClass) {
+                        this.ClassOrder.Add(car);
+                    }
+
                     if (car.IsFocused) {
                         // nothing to do
                     } else if (car.RelativeSplinePositionToFocusedCar > 0) {
@@ -96,7 +145,9 @@ namespace KLPlugins.DynLeaderboards {
                         this.RelativeOnTrackBehindOrder.Add(car);
                     }
                 }
+            }
 
+            if (this.FocusedCar != null) {
                 static int CmpCarByRelativeSplinePositionToFocusedCar(CarData c1, CarData c2) {
                     return c2.RelativeSplinePositionToFocusedCar.CompareTo(c1.RelativeSplinePositionToFocusedCar);
                 }

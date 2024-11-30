677f308bceace343bdde00ad7bbf948214316986
Kaius Loos <37976028+kaiusl@users.noreply.github.com>
2024-11-30 21:51:01 +0200

    refactor: rename unit tests project + add some more basic tests

--- KLPlugins.DynLeaderboards.Tests.Unit/Properties/AssemblyInfo.cs
+++ KLPlugins.DynLeaderboards.Tests.Unit/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KLPlugins.DynLeaderboards.Tests")]
+[assembly: AssemblyTitle("KLPlugins.DynLeaderboards.Tests.Unit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KLPlugins.DynLeaderboards.Tests")]
+[assembly: AssemblyProduct("KLPlugins.DynLeaderboards.Tests.Unit")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha.1")]
